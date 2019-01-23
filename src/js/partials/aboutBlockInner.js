e910eaf3612cb0f26cfba12fef1bb6d71d917cd9
Ilya <rickmortyand4@gmail.com>
2019-01-23 14:17:13 +0300

    Add cards block, speakers

--- src/js/partials/aboutBlockInner.js
+++ src/js/partials/aboutBlockInner.js
@@ -12,4 +12,16 @@ var innerBlock1 = '<div class="about-inner"><img src="img/aboutBlocks/rewards/el
                   'That means if you do not cross to the next elite level before the next event, you cannot claim your prizes.</p>' +
                   '<button class="about-button">Bring on the swag!</button></div><div>';
 
+var innerBlock2 = '<div class="about-inner sponsors-inner"><h2 class="about-title sponsors-title">Sponsor Our Event!</h2>' +
+                  '<p class="about-text sponsors-text">If you want to help us making this event lively and successful,' +
+                  'help us by sponsoring our event!<br>This will help us bring in more people, have better livestreaming' +
+                  'equipment, maybe even our own chef!<br>You can also sponsor us by giving away free swag at the event!<br><br>' +
+                  'Here’s who have sponsored us already:</p><div class="sponsors-logo"><div class="mountains-img">'+
+                  '<img src="img/aboutBlocks/sponsors/mountains.png" alt="mountains"><img src="img/aboutBlocks/sponsors/mountainsHouse.png" alt="house">' +
+                  '</div><img height="75px" src="img/aboutBlocks/sponsors/snake.png" alt="snake"><img height="28px" src="img/aboutBlocks/sponsors/evanto.png" alt="evanto">' + 
+                  '</div><button class="about-button sponsors-button">Sponsor Us!</button></div>'
+
 aboutBlocks[0].getElementsByClassName("container")[0].innerHTML = innerBlock1;
+
+aboutBlocks[1].getElementsByClassName("container")[0].innerHTML = innerBlock2;
+aboutBlocks[1].getElementsByClassName("container")[0].setAttribute('class', 'sponsors-container container');
